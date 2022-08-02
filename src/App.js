3e2bc17ead35c6f6417baa2574a32275c5a9f334
Yacoub <yacoubhaddad33@outlook.com>
2022-08-02 09:51:40 +0100

    test

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ import Projects from "./components/Projects";
 function App() {
   return (
     <div className="App">
-      <Nav />
-      <Landing />
-      <FeaturedProject
+      {/* <Nav />
+      <Landing /> */}
+      {/* <FeaturedProject
         title="Spotify"
         languages="html css js react"
         description="this is a text"
-      />
+      /> */}
       <Projects />
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
