12f4ccc2e3152b1ddd6a2fce4b8bf1d417913ab2
Yacoub <yacoubhaddad33@outlook.com>
2022-08-24 15:53:11 +0100

    fade effect added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Fade from "react-reveal/Fade";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import FeaturedProject from "./components/FeaturedProject";
@@ -10,16 +11,18 @@ function App() {
   return (
     <div className="App">
       <Nav />
-      <Landing />
-      <About />
-      <FeaturedProject
-        title="E-Commerce React Web App"
-        languages="HTML CSS JavaScript React"
-        description="An e-commerce website for books using a test book API where each book is mapped using reusable HTML elements."
-      />
-      <Projects />
-      <Contact />
-      <Footer />
+      <Fade>
+        <Landing />
+        <About />
+        <FeaturedProject
+          title="E-Commerce React Web App"
+          languages="HTML CSS JavaScript React"
+          description="An e-commerce website for books using a test book API where each book is mapped using reusable HTML elements."
+        />
+        <Projects />
+        <Contact />
+        <Footer />
+      </Fade>
     </div>
   );
 }
